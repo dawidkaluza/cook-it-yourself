4cfb8293d3b615707c5d4ebcf5adc5e18009535c
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-02 10:28:14 +0200

    test UserName

---
+++ user-service/service/src/test/java/pl/dkaluza/userservice/domain/UserNameTest.java
@@ -0,0 +1,83 @@
+package pl.dkaluza.userservice.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pl.dkaluza.domaincore.FieldError;
+import pl.dkaluza.domaincore.exceptions.ValidationException;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+
+class UserNameTest {
+    @ParameterizedTest
+    @MethodSource("invalidNameParamsProvider")
+    void of_invalidParams_throwException(String name) {
+        // Given, when
+        var e = catchThrowableOfType(
+            () -> UserName.of(name).produce(),
+            ValidationException.class
+        );
+
+        // Then
+        assertThat(e)
+            .isNotNull();
+
+        assertThat(e.getErrors())
+            .extracting(FieldError::name)
+            .containsExactly("name");
+    }
+
+    private static Stream<String> invalidNameParamsProvider() {
+        return Stream.of(
+            null,
+            "dd",
+            "d".repeat(129),
+            "   D    "
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validNameParamsProvider")
+    void of_validParams_returnObject(String name) {
+        // Given, when
+        var nameObject = UserName.of(name).produce();
+
+        // Then
+        assertThat(nameObject)
+            .isNotNull()
+            .extracting(UserName::getValue)
+            .isEqualTo(name);
+
+    }
+
+    private static Stream<String> validNameParamsProvider() {
+        return Stream.of(
+            "Daw",
+            "D D",
+            "D".repeat(128)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("equalParamsProvider")
+    void equal_variousObjects_returnExpectedResult(UserName first, Object second, boolean expectedResult) {
+        assertThat(first.equals(second))
+            .isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> equalParamsProvider() {
+        var userName1 = UserName.of("Dawid").produce();
+        var userName1Clone = UserName.of("Dawid").produce();
+        var username2 = UserName.of("Gabriel").produce();
+        return Stream.of(
+            Arguments.of(userName1, null, false),
+            Arguments.of(userName1, new Object(), false),
+            Arguments.of(userName1, username2, false),
+            Arguments.of(userName1, userName1, true),
+            Arguments.of(userName1, userName1Clone, true)
+        );
+    }
+}
