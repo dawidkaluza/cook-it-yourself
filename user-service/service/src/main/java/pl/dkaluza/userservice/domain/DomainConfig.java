748194cb63d86416f94d14bc297050d9da77248e
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-03 09:34:54 +0200

    "transactionalize" service methods

--- user-service/service/src/main/java/pl/dkaluza/userservice/domain/DomainConfig.java
+++ user-service/service/src/main/java/pl/dkaluza/userservice/domain/DomainConfig.java
@@ -1,14 +1,38 @@
 package pl.dkaluza.userservice.domain;
 
+import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.interceptor.NameMatchTransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionAttributeSource;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 import pl.dkaluza.userservice.ports.out.UserEventPublisher;
 import pl.dkaluza.userservice.ports.out.UserRepository;
 
 @Configuration
 class DomainConfig {
+    private final PlatformTransactionManager txManager;
+
+    DomainConfig(PlatformTransactionManager txManager) {
+        this.txManager = txManager;
+    }
+
     @Bean
     DefaultUserService defaultUserService(UserRepository userRepository, UserEventPublisher userEventPublisher) {
-        return new DefaultUserService(userRepository, userEventPublisher);
+        var userService = new DefaultUserService(userRepository, userEventPublisher);
+        var attrSource = new NameMatchTransactionAttributeSource();
+        attrSource.addTransactionalMethod("signUp", new DefaultTransactionAttribute());
+        return transactional(userService, attrSource);
+    }
+
+    private <T> T transactional(T service, TransactionAttributeSource attrSource) {
+        var proxyFactory = new ProxyFactory(service);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TransactionInterceptor((TransactionManager) txManager, attrSource));
+        //noinspection unchecked
+        return (T) proxyFactory.getProxy();
     }
 }
