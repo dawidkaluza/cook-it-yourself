b1ebb3e7ab71d30c9be690a4b2f16b805f3fc0e1
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-09 14:52:58 +0200

    impl uncaught exceptions handling in web adapter

--- user-service/service/src/main/java/pl/dkaluza/userservice/adapters/in/web/DefaultControllerAdvice.java
+++ user-service/service/src/main/java/pl/dkaluza/userservice/adapters/in/web/DefaultControllerAdvice.java
@@ -1,9 +1,33 @@
 package pl.dkaluza.userservice.adapters.in.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+
+@RestControllerAdvice
 class DefaultControllerAdvice extends ResponseEntityExceptionHandler {
-//     TODO customize it later (at best by adjusting all errors to RFC 7807 Problem Details)
+    private static final Logger logger = LoggerFactory.getLogger(DefaultControllerAdvice.class);
+
+    @ExceptionHandler(RuntimeException.class)
+    ResponseEntity<?> unexpectedExceptionHandler(RuntimeException exception) {
+        logger.error("Unexpected exception caught by controller advice", exception);
+
+        return ResponseEntity.internalServerError().body(
+            new ErrorResponse(
+                "Error within the server. Try again later.",
+                ZonedDateTime.now(ZoneOffset.UTC),
+                Collections.emptyList()
+            )
+        );
+    }
+
+    //     TODO customize it later (at best by adjusting all errors to RFC 7807 Problem Details)
 }
