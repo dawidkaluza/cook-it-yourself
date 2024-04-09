89595c88561e44e96be36688b148b72db0409890
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-09 13:25:37 +0200

    impl basic web error handler

--- user-service/service/src/main/java/pl/dkaluza/userservice/adapters/in/web/DefaultControllerAdvice.java
+++ user-service/service/src/main/java/pl/dkaluza/userservice/adapters/in/web/DefaultControllerAdvice.java
@@ -1,8 +1,9 @@
 package pl.dkaluza.userservice.adapters.in.web;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-class DefaultControllerAdvice {
-//     TODO customize error response for generic use cases (invalid req body, unauthenticated, etc.)
+class DefaultControllerAdvice extends ResponseEntityExceptionHandler {
+//     TODO customize it later (at best by adjusting all errors to RFC 7807 Problem Details)
 }
