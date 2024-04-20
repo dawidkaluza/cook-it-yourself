99e848ec4e0d369dcb927a4607360db2323d3274
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-20 15:26:45 +0200

    start impl signIn tests

--- user-service/service/src/integrationTest/java/pl/dkaluza/userservice/UserAuthenticationTest.java
+++ user-service/service/src/integrationTest/java/pl/dkaluza/userservice/UserAuthenticationTest.java
@@ -1,6 +1,29 @@
 package pl.dkaluza.userservice;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.Filter;
+import io.restassured.filter.FilterContext;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.FilterableRequestSpecification;
+import io.restassured.specification.FilterableResponseSpecification;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import pl.dkaluza.userservice.config.EnableTestcontainers;
+import pl.dkaluza.userservice.config.JdbiFacade;
+
+import java.time.Duration;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
 
 /*
 oauth2 auth code tests:
@@ -39,15 +62,71 @@ Summary:
 Rest Assured can try to simulate opening a page and sending a request, but it does not really do that so what is being tested is only service endpoints, not web UI - which is not bad since UI automated tests are not that straightforward, hence it might be better to have UI tests in e2e tests
 
  */
-class UserAuthenticationTest {
-    @Test
-    void signIn_invalidCsrfToken_returnForbidden() {
 
+@EnableTestcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserAuthenticationTest {
+    private JdbiFacade jdbiFacade;
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void beforeEach() {
+        jdbiFacade = new JdbiFacade();
+        jdbiFacade.start();
+
+        baseURI = "http://localhost:" + port;
+
+        var handle = jdbiFacade.getHandle();
+        handle.execute("DELETE FROM users");
+
+        var req = new RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .setBody(Map.of(
+                "email", "dawid@d.c",
+                "password", "password",
+                "name", "Dawid"
+            ))
+            .build();
+        var res = given().spec(req).post("/user/sign-up");
+        if (res.statusCode() != 201) {
+            throw new IllegalStateException("Sign up failed.\n > Status code: " + res.statusCode() + "\n > Body: " + res.body().asPrettyString());
+        }
     }
 
-    @Test
-    void signIn_invalidEmailOrPassword_returnUnauthorized() {
+    @AfterEach
+    void afterAll() {
+        jdbiFacade.stop();
+    }
 
+    @ParameterizedTest
+    @ValueSource(strings = { "123", "" })
+    void signIn_invalidCsrfToken_returnForbidden(String csrfToken) {
+        given()
+            .header("X-XSRF-TOKEN", csrfToken)
+            .formParam("username", "dawid@d.c")
+            .formParam("password", "password")
+        .when()
+            .post("/sign-in")
+        .then()
+            .statusCode(403);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "gabriel@d.c, 12345",
+//        "dawid@d.c, passwd",
+    })
+    void signIn_invalidEmailOrPassword_returnUnauthorized(String email, String password) {
+        given()
+            .filter(csrfCookieFilter("/web/sign-in"))
+            .formParam("username", email)
+            .formParam("password", password)
+        .when()
+            .post("/sign-in")
+        .then()
+            .statusCode(403);
     }
 
     @Test
@@ -78,6 +157,26 @@ class UserAuthenticationTest {
     @Test
     void signOut_authenticated_invalidateAuthentication() {
 
+    }
+
+    private Filter csrfCookieFilter(String path) {
+        return new CsrfCookieFilter(path);
+    }
+
+    private static class CsrfCookieFilter implements Filter {
+        private final String path;
+
+        private CsrfCookieFilter(String path) {
+            this.path = path;
+        }
+
+        @Override
+        public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
+            var res = given().auth().none().disableCsrf().cookies(requestSpec.getCookies()).get(path);
+            var token = res.getCookie("XSRF-TOKEN");
+            requestSpec.header("X-XSRF-TOKEN", token);
+            return ctx.next(requestSpec, responseSpec);
+        }
     }
 }
 
