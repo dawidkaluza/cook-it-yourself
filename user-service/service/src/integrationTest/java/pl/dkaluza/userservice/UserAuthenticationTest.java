e8994a54130d4f8f0df11f95b5fc61a387c127f7
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-22 14:24:51 +0200

    impl more signIn tests

--- user-service/service/src/integrationTest/java/pl/dkaluza/userservice/UserAuthenticationTest.java
+++ user-service/service/src/integrationTest/java/pl/dkaluza/userservice/UserAuthenticationTest.java
@@ -5,6 +5,8 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.Filter;
 import io.restassured.filter.FilterContext;
 import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Cookies;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
@@ -24,6 +26,8 @@ import pl.dkaluza.userservice.config.JdbiFacade;
 import pl.dkaluza.userservice.config.JedisFacade;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -111,17 +115,17 @@ class UserAuthenticationTest {
         jedisFacade.stop();
     }
 
-    // TODO doesnt make sense when user is unauthorized I guess
     @ParameterizedTest
     @ValueSource(strings = { "123", "" })
     void signIn_invalidCsrfToken_returnUnauthorized(String csrfToken) {
         given()
+            .filter(csrfCookieFilter("/web/sign-in"))
             .header("X-XSRF-TOKEN", csrfToken)
             .formParam("username", "dawid@d.c")
             .formParam("password", "password")
-            .when()
+        .when()
             .post("/sign-in")
-            .then()
+        .then()
             .statusCode(403);
     }
 
@@ -144,12 +148,8 @@ class UserAuthenticationTest {
     @Test
     void signIn_unauthenticated_authenticate() {
         // Given
-        var signInPage = given().get("/web/sign-in");
-        var sessionId = signInPage.getCookie("SESSION");
-        var xsrfToken = signInPage.getCookie("XSRF-TOKEN");
         var request = given()
-            .cookies(signInPage.getCookies())
-            .header("X-XSRF-TOKEN", xsrfToken)
+            .filter(csrfCookieFilter("/web/sign-in"))
             .formParam("username", "dawid@d.c")
             .formParam("password", "password");
 
@@ -161,25 +161,38 @@ class UserAuthenticationTest {
             .statusCode(200)
             .body("redirectUrl", notNullValue());
 
-        var newSessionId = response.getCookie("SESSION");
-        assertThat(newSessionId)
-            .isNotBlank()
-            .isNotEqualTo(sessionId);
+        var sessionId = response.getCookie("SESSION");
+        assertThat(sessionId)
+            .isNotBlank();
 
+        var decodedSessionId = new String(Base64.getDecoder().decode(sessionId));
         var jedis = jedisFacade.getJedis();
-        assertThat(jedis.keys("*" + sessionId))
-            .hasSize(0);
-        assertThat(jedis.keys("*" + newSessionId))
+        assertThat(jedis.keys("*" + decodedSessionId))
             .hasSize(1);
-
-        // TODO fix:
-        //  last assertion fails - cant find keys with new session id
-        //  in all previous tests, csrf token was not in cookies when sending sign in request, which is a mistake and should be fixed
     }
 
     @Test
     void signIn_redirectedToSignIn_authenticateAndRedirectBack() {
+        // Given
+        var oauthAuthorizeUrl = "/oauth2/authorize?response_type=code&client_id=ciy-web&state=1234xyz&code_challenge=MMRGwBwWyq4DLBuYbwPHRF6HGyVnN_UAUDnQ8GVGjn8&code_challenge_method=S256";
+        var oauthAuthorizeResponse = given()
+            .redirects().follow(false)
+            .contentType(ContentType.JSON)
+            .get(oauthAuthorizeUrl);
 
+        var request = given()
+            .filter(csrfCookieFilter("/web/sign-in", oauthAuthorizeResponse.detailedCookies()))
+            .formParam("username", "dawid@d.c")
+            .formParam("password", "password");
+
+        // When
+        var response = request.post("/sign-in");
+
+        // Then
+        response.then()
+            .statusCode(200)
+            .body("redirectUrl", containsString("/oauth2/authorize"))
+            .cookie("SESSION", not(blankOrNullString()));
     }
 
     @Test
@@ -202,20 +215,36 @@ class UserAuthenticationTest {
 
     }
 
+    private String base64Decode(String input) {
+        return new String(Base64.getDecoder().decode(input));
+    }
+
     private Filter csrfCookieFilter(String path) {
-        return new CsrfCookieFilter(path);
+        return new CsrfCookieFilter(path, new Cookies());
+    }
+
+    private Filter csrfCookieFilter(String path, Cookies cookies) {
+        return new CsrfCookieFilter(path, cookies);
     }
 
     private static class CsrfCookieFilter implements Filter {
         private final String path;
+        private final Cookies cookies;
 
-        private CsrfCookieFilter(String path) {
+        private CsrfCookieFilter(String path, Cookies cookies) {
             this.path = path;
+            this.cookies = cookies;
         }
 
         @Override
         public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext ctx) {
-            var res = given().auth().none().disableCsrf().cookies(requestSpec.getCookies()).get(path);
+            var res = given().cookies(cookies).get(path);
+
+            var cookiesList = new ArrayList<Cookie>();
+            cookiesList.addAll(cookies.asList());
+            cookiesList.addAll(res.getDetailedCookies().asList());
+            requestSpec.cookies(new Cookies(cookiesList));
+
             var token = res.getCookie("XSRF-TOKEN");
             requestSpec.header("X-XSRF-TOKEN", token);
             return ctx.next(requestSpec, responseSpec);
