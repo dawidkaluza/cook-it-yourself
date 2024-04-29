6db9541027a0b13af7795c69f604849c27dc38ac
Dawid Kałuża <dkaluza27@gmail.com>
2024-04-29 08:08:33 +0200

    init kitchen service

---
+++ kitchen-service/src/main/java/pl/dkaluza/kitchenservice/KitchenServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.dkaluza.kitchenservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class KitchenServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenServiceApplication.class, args);
+    }
+}
