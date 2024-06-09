1a297f2f95a170eae349d505622decba44c00eff
Dawid Kałuża <dkaluza27@gmail.com>
2024-06-09 15:29:09 +0200

    change btn icon

--- web-app/app/my-recipes/add/_components/FormButtons.tsx
+++ web-app/app/my-recipes/add/_components/FormButtons.tsx
@@ -3,7 +3,7 @@ const FormButtons = () => {
     <div className="row mb-4">
       <div className="col-12 text-center">
         <div className="btn-group">
-          <button className="btn btn-primary" type="button">
+          <button className="btn btn-primary" type="submit">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                  className="bi bi-check2" viewBox="0 0 16 16">
               <path
@@ -12,11 +12,14 @@ const FormButtons = () => {
 
             {" Submit "}
           </button>
-          <button className="btn btn-outline-secondary" type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                 className="bi bi-backspace-reverse-fill" viewBox="0 0 16 16">
+          <button className="btn btn-outline-secondary" type="reset">
+            <svg
+              xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              className="bi bi-x" viewBox="0 0 16 16"
+            >
               <path
-                d="M0 3a2 2 0 0 1 2-2h7.08a2 2 0 0 1 1.519.698l4.843 5.651a1 1 0 0 1 0 1.302L10.6 14.3a2 2 0 0 1-1.52.7H2a2 2 0 0 1-2-2zm9.854 2.854a.5.5 0 0 0-.708-.708L7 7.293 4.854 5.146a.5.5 0 1 0-.708.708L6.293 8l-2.147 2.146a.5.5 0 0 0 .708.708L7 8.707l2.146 2.147a.5.5 0 0 0 .708-.708L7.707 8z"/>
+                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
+              />
             </svg>
             {" Reset "}
           </button>
@@ -26,4 +29,4 @@ const FormButtons = () => {
   );
 };
 
-export { FormButtons };
+export {FormButtons};
