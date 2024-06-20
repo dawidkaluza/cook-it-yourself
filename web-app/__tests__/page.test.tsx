25b131a68ae1a543ae1c3937f72eda3c4dfd4744
Dawid Kałuża <dkaluza27@gmail.com>
2024-06-20 15:27:59 +0200

    add snapshot test

--- web-app/__tests__/page.test.tsx
+++ web-app/__tests__/page.test.tsx
@@ -2,9 +2,14 @@ import { expect, test } from 'vitest'
 import {render, screen} from "@testing-library/react";
 import Page from "@/app/page";
 
-test('Page', () => {
+test('Page has welcome message', () => {
   render(<Page />);
 
   const welcomeText = screen.queryByText(/Welcome my cook/);
   expect(welcomeText).toBeDefined();
 });
+
+test('Page matches existing snapshot', () => {
+  const page = render(<Page />);
+  expect(page.container).toMatchSnapshot();
+})
