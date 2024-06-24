419eaf46643e4f394ca82af0403d5dbbeeb9c8ef
Dawid Kałuża <dkaluza27@gmail.com>
2024-06-24 20:42:34 +0200

    add Main tests

---
+++ web-app/__tests__/_components/Main.test.tsx
@@ -0,0 +1,20 @@
+import {describe, expect, test} from "vitest";
+import {render, screen} from "@testing-library/react";
+import {Main} from "@/app/_components/Main";
+
+describe("Main component", () => {
+  test("renders", () => {
+    // Given, when
+    const menu = render(<Main><p>Main page</p></Main>);
+
+    // Then
+    const main = screen.queryByRole("main");
+    expect(main).not.toBeNull();
+
+    const p = screen.queryByText(/Main page/);
+    expect(p).not.toBeNull();
+
+    expect(menu.container).toMatchSnapshot();
+    menu.unmount();
+  });
+});
