a02d2aa41b879e1d133e3b862a6b12c442c9490b
Dawid Kałuża <dkaluza27@gmail.com>
2024-07-19 09:58:23 +0200

    impl ReviewRecipe tests

---
+++ web-app/__tests__/my-recipes/[id]/_components/ReviewRecipe.test.tsx
@@ -0,0 +1,74 @@
+import {describe, expect, Mock, test, vi} from "vitest";
+import {reviewRecipe} from "@/app/my-recipes/[id]/actions";
+import {render, screen} from "@testing-library/react";
+import {ReviewRecipe} from "@/app/my-recipes/[id]/_components/ReviewRecipe";
+import {RecipeDetails} from "@/app/my-recipes/_dtos/recipe";
+
+vi.mock("@/app/my-recipes/[id]/actions", () => {
+  return {
+    reviewRecipe: vi.fn(),
+  };
+});
+
+describe("ReviewRecipe component", () => {
+  test("renders given recipe", async () => {
+    // Given
+    const recipe : RecipeDetails = {
+      id: 1,
+      name: "Boiled sausages",
+      description: "How to boil delicious sausages",
+      ingredients: [
+        {
+          id: 1,
+          name: "Sausage",
+          value: "3",
+          measure: "pc",
+        }
+      ],
+      methodSteps: [
+        {
+          id: 1,
+          text: "Boil sausages for about 3 mins",
+        }
+      ],
+      cookingTime: 3,
+      portionSize: {
+        value: "1",
+        measure: "plate",
+      }
+    };
+    (reviewRecipe as Mock).mockResolvedValue(recipe);
+
+    // When
+    const reviewComponent = render(await ReviewRecipe({ id: 1 }));
+
+    // Then
+    const nameHeader = screen.queryByRole("heading", { name: /Boiled sausages/ });
+    expect(nameHeader).not.toBeNull();
+
+    const descHeader = screen.queryByRole("heading", { name: /How to boil delicious sausages/ });
+    expect(descHeader).not.toBeNull();
+
+    const portionSizeElement = screen.queryByText("Portion size: " + Number(recipe.portionSize.value) + " " + recipe.portionSize.measure);
+    expect(portionSizeElement).not.toBeNull();
+
+    const cookingTimeElement = screen.queryByText("Cooking time: " + recipe.cookingTime + " minutes");
+    expect(cookingTimeElement).not.toBeNull();
+
+    for(const ingredient of recipe.ingredients) {
+      const ingredientElement = screen.queryByText(
+        ingredient.name + " " + Number(ingredient.value) + " " + ingredient.measure
+      );
+
+      expect(ingredientElement).not.toBeNull();
+    }
+
+    for(const step of recipe.methodSteps) {
+      const stepElement = screen.queryByText(step.text);
+      expect(stepElement).not.toBeNull();
+    }
+
+    expect(reviewComponent.container).toMatchSnapshot();
+    reviewComponent.unmount();
+  });
+});
