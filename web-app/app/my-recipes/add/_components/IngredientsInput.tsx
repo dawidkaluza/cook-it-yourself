23ed4aae2487ddd20129276a52a66777ff8a4581
Dawid Kałuża <dkaluza27@gmail.com>
2024-07-26 13:44:27 +0200

    make the whole ingredient controlled component

--- web-app/app/my-recipes/add/_components/IngredientsInput.tsx
+++ web-app/app/my-recipes/add/_components/IngredientsInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import {FieldError} from "@/app/my-recipes/add/actions";
 
 type Props = {
@@ -17,6 +17,13 @@ type Ingredient = {
 const IngredientsInput = ({ fieldErrors }: Props) => {
   const [newIngredient, setNewIngredient] = useState("");
   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
+  const [generatedId, setGeneratedId] = useState(0);
+
+  const generateNewId = () => {
+    const newId = generatedId + 1;
+    setGeneratedId(newId);
+    return newId;
+  }
 
   const addNewIngredient = () => {
     if (newIngredient.trim().length === 0) {
@@ -24,7 +31,7 @@ const IngredientsInput = ({ fieldErrors }: Props) => {
     }
 
     const newIngredients = [...ingredients];
-    const newIngredientId = newIngredients.length + 1;
+    const newIngredientId = generateNewId();
 
     const lastSpaceIndex = newIngredient.lastIndexOf(" ");
     if (lastSpaceIndex === -1 || newIngredient.slice(lastSpaceIndex).search(/ \d/g) !== 0) {
@@ -73,56 +80,18 @@ const IngredientsInput = ({ fieldErrors }: Props) => {
       <div className="col-sm-10">
         {ingredients.map(ingredient => (
           <div key={ingredient.id} className="row">
-            <div className="input-group">
-              <input
-                name="ingredientName" defaultValue={ingredient.name}
-                className="form-control"
-                placeholder="Name" style={{minWidth: "60%"}}
-              />
-
-              <input
-                name="ingredientAmount" defaultValue={ingredient.amount}
-                className="form-control"
-                placeholder="Amount" style={{minWidth: "20%"}}
-              />
-
-              <input
-                name="ingredientUnit" defaultValue={ingredient.unit}
-                className="form-control"
-                placeholder="Unit"
-              />
-
-              <button className="btn btn-outline-danger" type="button" onClick={() => deleteIngredient(ingredient.id)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     className="bi bi-dash-lg" viewBox="0 0 16 16">
-                  <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"/>
-                </svg>
-              </button>
-            </div>
+            <IngredientFields
+              ingredient={ingredient}
+              onDelete={() => deleteIngredient(ingredient.id)}
+            />
           </div>
         ))}
         <div className="row">
-          <div className="input-group">
-            <input
-              name="newIngredient" id="newIngredient" className="form-control"
-              value={newIngredient}
-              placeholder="Name and optionally amount (paprikas 3, sugar 250g, etc.)"
-              onChange={(e) => setNewIngredient(e.currentTarget.value)}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  e.preventDefault();
-                  addNewIngredient();
-                }
-              }}
-            />
-            <button className="btn btn-success" type="button" onClick={() => addNewIngredient()}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                   className="bi bi-plus-lg" viewBox="0 0 16 16">
-                <path fillRule="evenodd"
-                      d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
-              </svg>
-            </button>
-          </div>
+          <NewIngredientFields
+            value={newIngredient}
+            onChange={setNewIngredient}
+            onSubmit={addNewIngredient}
+          />
         </div>
         <div className="row">
           {fieldErrors && fieldErrors.map(fieldError => {
@@ -135,6 +104,86 @@ const IngredientsInput = ({ fieldErrors }: Props) => {
         </div>
       </div>
     </div>
+  );
+};
+
+const IngredientFields = (props: { ingredient: Ingredient, onDelete: () => void }) => {
+  const { ingredient, onDelete } = props;
+  const [ name, setName ] = useState(ingredient.name);
+  const [ amount, setAmount ] = useState(ingredient.amount);
+  const [ unit, setUnit ] = useState(ingredient.unit);
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("Change name to: " + event.target.value);
+    setName(event.target.value);
+  };
+
+  return (
+    <div className="input-group">
+      <input
+        name="ingredientName"
+        value={name}
+        onChange={handleNameChange}
+        className="form-control"
+        placeholder="Name" style={{minWidth: "60%"}}
+      />
+
+      <input
+        name="ingredientAmount"
+        value={amount}
+        onChange={(event) => setAmount(Number(event.target.value))}
+        className="form-control"
+        placeholder="Amount" style={{minWidth: "20%"}}
+      />
+
+      <input
+        name="ingredientUnit"
+        value={unit}
+        onChange={(event) => setUnit(event.target.value)}
+        className="form-control"
+        placeholder="Unit"
+      />
+
+      <button className="btn btn-outline-danger" type="button" onClick={onDelete}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+           className="bi bi-dash-lg" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"/>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+const NewIngredientFields = (
+  props: {
+    value: string,
+    onChange: (value: string) => void,
+    onSubmit: () => void,
+  }
+) => {
+  const { value, onChange, onSubmit } = props;
+  return (
+    <>
+      <input
+        name="newIngredient" id="newIngredient" className="form-control"
+        value={value}
+        placeholder="Name and optionally amount (paprikas 3, sugar 250g, etc.)"
+        onChange={(e) => onChange(e.currentTarget.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            onSubmit();
+          }
+        }}
+      />
+      <button className="btn btn-success" type="button" onClick={onSubmit}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+             className="bi bi-plus-lg" viewBox="0 0 16 16">
+          <path fillRule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+        </svg>
+      </button>
+    </>
   );
 };
 
