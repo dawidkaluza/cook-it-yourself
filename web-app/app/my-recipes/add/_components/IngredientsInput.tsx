587a0ee1f844976eda257a1310525369b8484ed4
Dawid Kałuża <dkaluza27@gmail.com>
2024-07-29 20:42:32 +0200

    bugfix styling

--- web-app/app/my-recipes/add/_components/IngredientsInput.tsx
+++ web-app/app/my-recipes/add/_components/IngredientsInput.tsx
@@ -158,7 +158,7 @@ const NewIngredientFields = (
 ) => {
   const { value, onChange, onSubmit } = props;
   return (
-    <>
+    <div className="input-group">
       <input
         name="newIngredient" id="newIngredient" className="form-control"
         value={value}
@@ -178,7 +178,7 @@ const NewIngredientFields = (
                 d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
         </svg>
       </button>
-    </>
+    </div>
   );
 };
 
