9f85f429d99fe2fce98b99a1ff90900ae96edb1c
Dawid Kałuża <dkaluza27@gmail.com>
2024-08-15 12:31:37 +0200

    adjust project config to be working with service

--- user-service/vite/vite.config.ts
+++ user-service/vite/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 9090,
+    strictPort: true,
+  },
+  preview: {
+    port: 9090,
+    strictPort: true,
+  },
   plugins: [react()],
 })
