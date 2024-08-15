133e2fce7c4c54e9a91c9795c5ac98784ee3aff2
Dawid Kałuża <dkaluza27@gmail.com>
2024-08-15 14:54:22 +0200

    add and initially config react router

--- user-service/vite/src/main.tsx
+++ user-service/vite/src/main.tsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import {SignUp} from "./pages/SignUp.tsx";
+import {SignIn} from "./pages/SignIn.tsx";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigate to={"/sign-in"} />
+    },
+    {
+      path: "/sign-up",
+      element: <SignUp />
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />
+    },
+    {
+      path: "/consent",
+      element: <App />
+    },
+  ],
+  {
+    basename: import.meta.env.VITE_PUBLIC_PATH
+  }
+);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <RouterProvider router={router} />,
+);
---
+++ user-service/vite/src/pages/SignUp.tsx
@@ -0,0 +1,6 @@
+
+export const SignUp = () => {
+  return (
+    <h1>Sign up page</h1>
+  )
+};
