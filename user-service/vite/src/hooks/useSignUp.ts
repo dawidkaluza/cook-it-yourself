e52487f55018a72ef86f74846eee16c63762a19e
Dawid Kałuża <dkaluza27@gmail.com>
2024-08-22 12:05:50 +0200

    create first version of sign up page

---
+++ user-service/vite/src/hooks/useSignUp.ts
@@ -0,0 +1,48 @@
+import {useCallback, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {userService} from "../domain/userService.ts";
+import {SignUpRequest} from "../domain/dtos/user.ts";
+import {InvalidFieldsError} from "../domain/errors/user.tsx";
+
+export const useSignUp = () => {
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const [fieldsErrors, setFieldsErrors] = useState<Record<string, string>>({});
+  const navigate = useNavigate();
+
+  const signUp = useCallback((request: SignUpRequest) => {
+    setLoading(true);
+    setSuccess("");
+    setError("");
+    setFieldsErrors({});
+
+    userService.signUp(request)
+      .then(() => {
+        setSuccess("Signed up successfully. You will be redirected to sign in page...");
+        setTimeout(() => navigate("/sign-in"), 800);
+      })
+      .catch(error => {
+        if (error instanceof InvalidFieldsError) {
+          setFieldsErrors(error.fields);
+        } else {
+          setError("Unable to process the request. Try again later.");
+          console.error("Unable to process the request.", error);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [
+    userService.signUp, setSuccess, setTimeout, navigate,
+    setFieldsErrors, setError, console.error, setLoading
+  ]);
+
+  return {
+    signUp,
+    loading,
+    error,
+    fieldsErrors,
+    success
+  }
+};
--- user-service/vite/src/pages/sign-up/SignUpPage.tsx
+++ user-service/vite/src/pages/sign-up/SignUpPage.tsx
@@ -1,5 +1,85 @@
+import {TextField} from "../../components/TextField.tsx";
+import {SubmitButton} from "../../components/SubmitButton.tsx";
+import {Link} from "../../components/Link.tsx";
+import {useSignUp} from "../../hooks/useSignUp.ts";
+import {ChangeEvent, FormEvent, useState} from "react";
+
 export const SignUpPage = () => {
+  const [ fields, setFields ] = useState({ email: "", name: "", password: "" });
+  const { signUp, loading, error, fieldsErrors, success } = useSignUp();
+
+  const onFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    setFields({
+      ...fields,
+      [target.name]: target.value
+    });
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    signUp(fields);
+  };
+
   return (
-    <h1>Sign up</h1>
+    <form
+      className="flex flex-col items-center max-w-sm mx-auto mt-8"
+      onSubmit={handleSubmit}
+    >
+      <h1
+        className="mb-4 text-center text-2xl"
+      >
+        Sign up
+      </h1>
+
+      <TextField
+        name="email"
+        label="E-mail"
+        value={fields.email}
+        error={fieldsErrors.email}
+        onChange={onFieldChange}
+        style="w-full mb-3"
+      />
+
+      <TextField
+        name="name"
+        label="Dawid"
+        value={fields.name}
+        error={fieldsErrors.name}
+        onChange={onFieldChange}
+        style="w-full mb-3"
+      />
+
+      <TextField
+        name="password"
+        label="Password"
+        type="password"
+        value={fields.password}
+        error={fieldsErrors.email}
+        onChange={onFieldChange}
+        style="w-full mb-3"
+      />
+
+      <SubmitButton loading={loading} style="mb-5">Sign up</SubmitButton>
+
+      <p>
+        Already signed up? {" "}
+        <Link to="/sign-in">
+          Sign in.
+        </Link>
+      </p>
+
+      {error &&
+        <p className="text-red-600">
+          {error}
+        </p>
+      }
+
+      {success &&
+        <p className="text-green-600">
+          {success}
+        </p>
+      }
+    </form>
   );
 };
